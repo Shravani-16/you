71828af17c328764e3ab5f949df07e515949020c
Shravani <148606408+Shravani-16@users.noreply.github.com>
2024-03-28 18:50:21 +0530

    subtractor

--- hello.java
+++ hello.java
@@ -29,39 +29,67 @@ class Simple {
 
     public static void main(String args[]) {
 
-        for (int i = 1; i < 4; i++) {
-            for (int a = 1; a < i + 1; a++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        // for (int i = 1; i < 4; i++) {
+        // for (int a = 1; a < i + 1; a++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+        // subtractor
+        System.out.println(substractBinaryNumber(10, 11));
+
+    }
+
+    static Integer addBinaryNumber(Integer firstNum, Integer secondNum) {
+        StringBuilder output = new StringBuilder();
+        int carry = 0;
+        int temp;
+        while (firstNum != 0 || secondNum != 0) {
+            temp = (firstNum % 10 + secondNum % 10 + carry) % 2;
+            output.append(temp);
+
+            carry = (firstNum % 10 + secondNum % 10 + carry) / 2;
+            firstNum = firstNum / 10;
+            secondNum = secondNum / 10;
         }
-        // i = 1
-        // a = 1
-        // print 1
-        // a++ a=2
-        // a print 2
-        // a++ 3
-        // a print 3
-        // a++ 4!<4 out of loop
-        // i++
-        // i=2
+        if (carry != 0) {
+            output.append(carry);
+        }
+        return Integer.valueOf(output.reverse().toString());
+    }
 
-        // inner loop(same process {
-        // a = 1
-        // print 1
-        // a++ a=2
-        // a print 2
-        // a++ 3
-        // a print 3
-        // a++ 4!<4 out of loop
-        // })
-        // i++
-        // i=3
-        // same wjbjh
-        // i++
-        // i= 4
-        // i!< 4
-        // out of outer loop
+    static Integer getOnesComplement(Integer secondnum) {
+        StringBuilder onesComplement = new StringBuilder();
+        while (secondnum > 0) {
+            int lastDigit = secondnum % 10;
+            if (lastDigit == 0) {
+                onesComplement.append(1);
+            } else {
+                onesComplement.append(0);
+            }
+            secondnum = secondnum / 10;
+        }
+        return Integer.valueOf(onesComplement.reverse().toString());
+    }
 
+    static Integer substractBinaryNumber(Integer firstNum, Integer secondNum) {
+        int onesComplement = Integer.valueOf(getOnesComplement(secondNum));
+        StringBuilder output = new StringBuilder();
+        int carry = 0;
+        int temp;
+        while (firstNum != 0 || onesComplement != 0) {
+            temp = (firstNum % 10 + onesComplement % 10 + carry) % 2;
+            output.append(temp);
+            carry = (firstNum % 10 + onesComplement % 10 + carry) / 2;
+
+            firstNum = firstNum / 10;
+            onesComplement = onesComplement / 10;
+        }
+        String additionOfFirstNumAndOnesComplement = output.reverse().toString();
+        if (carry == 1) {
+            return addBinaryNumber(Integer.valueOf(additionOfFirstNumAndOnesComplement), carry);
+        } else {
+            return getOnesComplement(Integer.valueOf(additionOfFirstNumAndOnesComplement));
+        }
     }
 }
